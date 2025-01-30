5fc66d6c3465066d0a473fd0f4bb5f148bbd7607
Ygor Nacif <ygor.nacif00@gmail.com>
2025-01-30 17:30:40 -0300

    Patient documentation controller finish

--- patient-api/src/main/java/com/ygornacif/patient_api/controller/PatientController.java
+++ patient-api/src/main/java/com/ygornacif/patient_api/controller/PatientController.java
@@ -2,9 +2,15 @@ package com.ygornacif.patient_api.controller;
 
 import com.ygornacif.patient_api.constants.ApiConstants;
 import com.ygornacif.patient_api.constants.PatientConstants;
+import com.ygornacif.patient_api.dto.ErrorResponseDto;
 import com.ygornacif.patient_api.dto.PatientDto;
 import com.ygornacif.patient_api.dto.ResponseDto;
 import com.ygornacif.patient_api.service.IPatientService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
@@ -26,14 +32,49 @@ public class PatientController {
 
     private final IPatientService patientService;
 
+    @Operation(
+            summary = "Create Patient REST API",
+            description = "REST API to create new Patient inside BeHealthyNutrition"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "201",
+                    description = "HTTP Status CREATED"
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP Status Internal Server Error",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponseDto.class)
+                    )
+            )
+    }
+    )
     @PostMapping
     public ResponseEntity<ResponseDto> createPatient(@Validated @RequestBody PatientDto patientDto) {
         patientService.createPatient(patientDto);
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(new ResponseDto(ApiConstants.STATUS_201, PatientConstants.MESSAGE_201));
     }
-    
-    
+
+
+    @Operation(
+            summary = "Fetch Patient Details REST API",
+            description = "REST API to fetch Patient details based on a mobile number"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "HTTP Status OK"
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP Status Internal Server Error",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponseDto.class)
+                    )
+            )
+    })
     @GetMapping
     public ResponseEntity<PatientDto> fetchPatient(@RequestParam @Pattern(regexp="(^$|[0-9]{11})",message = "Mobile number must be 11 digits")
                                                     String mobileNumber) {
@@ -41,19 +82,71 @@ public class PatientController {
         return ResponseEntity.status(HttpStatus.OK).body(patientDto);
     }
 
+    @Operation(
+            summary = "Update Patient Details REST API",
+            description = "REST API to update Patient details based on a email"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "HTTP Status OK"
+            ),
+            @ApiResponse(
+                    responseCode = "417",
+                    description = "Expectation Failed"
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP Status Internal Server Error",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponseDto.class)
+                    )
+            )
+    })
     @PutMapping("/{id}")
     public ResponseEntity<ResponseDto> updatePatient(
             @Validated @RequestBody PatientDto patientDto,
             @PathVariable Long id) {
-        patientService.updatePatient(patientDto);
-        return ResponseEntity.status(HttpStatus.OK)
-                .body(new ResponseDto(ApiConstants.STATUS_200, ApiConstants.MESSAGE_200_PROCESSED));
+        boolean isUpdated = patientService.updatePatient(patientDto);
+        if (isUpdated) {
+            return ResponseEntity.status(HttpStatus.OK)
+                    .body(new ResponseDto(ApiConstants.STATUS_200, ApiConstants.MESSAGE_200_PROCESSED));
+        } else {
+            return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED)
+                    .body(new ResponseDto(ApiConstants.STATUS_417, ApiConstants.MESSAGE_417_UPDATE_FAILED));
+        }
     }
 
+    @Operation(
+            summary = "Delete Patient Details REST API",
+            description = "REST API to delete Patient details based on a id"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "HTTP Status OK"
+            ),
+            @ApiResponse(
+                    responseCode = "417",
+                    description = "Expectation Failed"
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP Status Internal Server Error",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponseDto.class)
+                    )
+            )
+    })
     @DeleteMapping("/{id}")
     public ResponseEntity<ResponseDto> deletePatient(@PathVariable Long id) {
-        patientService.deletePatient(id);
-        return ResponseEntity.status(HttpStatus.OK)
-                .body(new ResponseDto(ApiConstants.STATUS_200, "Patient deleted successfully."));
+        boolean isDeleted = patientService.deletePatient(id);
+        if (isDeleted) {
+            return ResponseEntity.status(HttpStatus.OK)
+                    .body(new ResponseDto(ApiConstants.STATUS_200, ApiConstants.MESSAGE_200_PROCESSED));
+        } else {
+            return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED)
+                    .body(new ResponseDto(ApiConstants.STATUS_417, ApiConstants.MESSAGE_417_DELETE_FAILED));
+        }
     }
 }
